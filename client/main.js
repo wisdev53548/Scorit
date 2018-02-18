62996174d4cad0e227c1b24de0ae010b031efdcc
Tom Smith <wisdev53548@hotmail.com>
2018-02-18 14:48:45 -0600

    Added +, -, and BONUS buttons and functionality

--- client/main.js
+++ client/main.js
@@ -9,6 +9,19 @@ const renderPlayers = playerList => {
     return (
       <h3 key={player._id}>
         {player.name} has {player.score} point(s)
+
+        <button onClick={() => {
+          Players.update(player._id, {$inc: {score: 1}})
+        }}>+1</button>
+
+        <button onClick={() => {
+          Players.update(player._id, {$inc: {score: -1}})
+        }}>-1</button>
+
+        <button onClick={() => {
+          Players.update(player._id, {$inc: {score: 100}})
+        }}>BONUS</button>
+
         <button onClick={() => Players.remove({_id: player._id})}>Remove</button>
       </h3>
     )
