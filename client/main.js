0ed9c39f669dd8434cf18479286c7b2bc59aa31d
Tom Smith <wisdev53548@hotmail.com>
2018-02-17 19:54:41 -0600

    Initial React implement and user greeting

--- client/main.js
+++ client/main.js
@@ -1,4 +1,11 @@
-import { greetUser } from './../imports/utils'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Meteor } from 'meteor/meteor'
 
-console.log('Log from /client/main.js')
-console.log(greetUser())
+Meteor.startup(() => {
+  let titleGreeting = <h1>Hello, and welcome to Skrit!</h1>
+  let userName = 'Jack'
+  let userGreeting = <p>Glad to see you back, {userName}!</p>
+  ReactDOM.render(titleGreeting, document.getElementById('app'))
+  ReactDOM.render(userGreeting, document.getElementById('greeting'))
+})
