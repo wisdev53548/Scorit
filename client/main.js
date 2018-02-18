f1bf47de9e96449da699518fda63bc6207089ebc
Tom Smith <wisdev53548@hotmail.com>
2018-02-17 21:31:59 -0600

    Dynamically rendering static players list

--- client/main.js
+++ client/main.js
@@ -2,14 +2,39 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Meteor } from 'meteor/meteor'
 
+const players = [
+  {
+    _id: 1,
+    name: 'Tom',
+    score: 100
+  },
+  {
+    _id: 2,
+    name: 'Jack',
+    score: 93
+  },
+  {
+    _id: 3,
+    name: 'Michael',
+    score: 62
+  }
+]
+
+const renderPlayers = playerList => {
+  return playerList.map(player => {
+    return <h3 key={player._id}>{player.name} has {player.score} point(s)</h3>
+  })
+}
+
 Meteor.startup(() => {
   let title = <h1>Scorit!</h1>
   let userName = 'Jack'
   let jsx = (
     <div>
-      <h1>{title}</h1>
+      {title}
       <h2>Good to see ya, {userName}</h2>
-      <p>This is the world's #1 scoring app</p>
+      <p>Here are the scores so far:</p>
+      {renderPlayers(players)}
     </div>
   )
   ReactDOM.render(jsx, document.getElementById('app'))
