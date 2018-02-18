6e9a4878733e5d31d2a43ebac821f9a6fee8ebb2
Tom Smith <wisdev53548@hotmail.com>
2018-02-18 09:12:24 -0600

    Added MiniMongo connection to client-side js

--- client/main.js
+++ client/main.js
@@ -2,30 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Meteor } from 'meteor/meteor'
 import { Tracker } from 'meteor/tracker'
-
 import { Players } from './../imports/api/players'
-
-Tracker.autorun(() => {
-  console.log('Player list', Players.find().fetch())
-})
-
-const players = [
-  {
-    _id: 1,
-    name: 'Tom',
-    score: 100
-  },
-  {
-    _id: 2,
-    name: 'Jack',
-    score: 93
-  },
-  {
-    _id: 3,
-    name: 'Michael',
-    score: 62
-  }
-]
 
 const renderPlayers = playerList => {
   return playerList.map(player => {
@@ -34,15 +11,22 @@ const renderPlayers = playerList => {
 }
 
 Meteor.startup(() => {
-  let title = <h1>Scorit!</h1>
-  let userName = 'Jack'
-  let jsx = (
-    <div>
-      {title}
-      <h2>Good to see ya, {userName}</h2>
-      <p>Here are the scores so far:</p>
-      {renderPlayers(players)}
-    </div>
-  )
-  ReactDOM.render(jsx, document.getElementById('app'))
+  Tracker.autorun(() => {
+    const players = Players.find().fetch()
+    const title = 'Scorit!'
+    let userName = 'Jack'
+    let jsx = (
+      <div>
+        <h1>{title}</h1>
+        <h2>Good to see ya, {userName}</h2>
+        <p>Here are the scores so far:</p>
+        {renderPlayers(players)}
+      </div>
+    )
+    ReactDOM.render(jsx, document.getElementById('app'))
+  })
+  // Players.insert({
+  //   name: 'Landon',
+  //   score: 88
+  // })
 })
